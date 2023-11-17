b88a2e3d6a370c939e5108910817e066a48af3dd
Mirko-Genovesi <mirkogenovesi286@gmail.com>
2023-11-17 11:05:27 -0300

    Update punto3.c

--- Lenguajes/C/Tp 2/punto3.c
+++ Lenguajes/C/Tp 2/punto3.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
 
+#define QUAN 10
 
-void user_input(float temp[10]){
-    for(int i=0; i<11; i++){
+
+void user_input(float temp[QUAN]){
+    for(int i=0; i<QUAN; i++){
         scanf("%f", &temp[i]);
     }
 }
 
 
+float min_temp(float temp[QUAN]){
+    float minimal_temp = temp[0];
+    for(int i=0; i<QUAN; i++){
+        if (temp[i]<minimal_temp){
+            minimal_temp = temp[i];
+        }
+    }
+    return minimal_temp;
+}
+
+
+float max_temp(float temp[QUAN]){
+    float maximum_temp = temp[0];
+    for(int i=0; i<QUAN; i++){
+        if (temp[i]>maximum_temp){
+            maximum_temp = temp[i];
+        }
+    }
+    return maximum_temp;
+}
+
+
+float avr_temp(float temp[QUAN]){
+    float average_temp = 0;
+    for (int i=0; i<QUAN; i++){
+        average_temp += temp[i];
+    }
+    average_temp /= QUAN;
+    return average_temp;
+}
+
+
 
 void main(){
-    float temp[10] = {0};
+    float temp[QUAN] = {0};
     user_input(temp);
+    float minimal_temp = min_temp(temp);
+    float maximum_temp = max_temp(temp);
+    float average_temp = avr_temp(temp);
+
 }
